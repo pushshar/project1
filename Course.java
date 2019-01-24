fcf5bb98d0a23fc1386a0e01509f0188880b2569
pushshar <46920213+pushshar@users.noreply.github.com>
2019-01-24 13:56:58 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,19 @@
+package com.capgemini.beans;
+
+public class Course {
+
+	private int id;
+	private String cname;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+}
